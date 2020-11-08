9ebc4be7d4a5b3d08449a87ea94c1b8c32f99eda
lorenzo-damico <loredam@hotmail.it>
2020-11-08 12:11:53 +0100

    Aggiunte migrations

---
+++ database/migrations/2020_11_08_104301_create_users_info_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->date('date_of_birth')->nullable();
+            $table->text('description')->nullable();
+            $table->string('website')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_info');
+    }
+}
