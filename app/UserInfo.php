918432b359082df9e9f63afa0098414c6632c85e
lorenzo-damico <loredam@hotmail.it>
2020-11-08 12:30:26 +0100

    Aggiunti model con relazioni fra le tabelle

---
+++ app/UserInfo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserInfo extends Model
+{
+    protected $table = "users_info";
+
+    public function user() {
+
+      return $this->belongsTo("App\User");
+    }
+}
